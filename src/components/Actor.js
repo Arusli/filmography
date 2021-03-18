279dbfad2825edb7659d0e642c7f76e88ae29b8e
Andrew Rusli <andreworusli@gmail.com>
2021-03-18 14:47:50 -0400

    some slight refactoring

--- src/components/Actor.js
+++ src/components/Actor.js
@@ -8,94 +8,13 @@ const blank = 'https://cdn-d8.nypl.org/s3fs-public/styles/hero_header_focal_poin
 class Actor extends React.Component {
 
     state = {
-        actorId: this.props.actorId,
-        actorImage: this.props.profilePath,
-        actorBio: '',
-        actorName: this.props.actorName,
-        profileUrl: this.props.profilePath,
-        filmArray: []
+        // actorId: this.props.actorId,
+        // actorImage: this.props.profilePath,
+        // actorBio: '',
+        // actorName: this.props.actorName,
+        // profileUrl: this.props.profilePath,
+        // filmArray: []
     }
-
-    // changeDefaultImage = async () => {
-    //     const imageResponse = await axios.get(`https://api.themoviedb.org/3/person/${this.state.actorId}/images`, {
-    //         params: {
-    //             api_key: key,
-    //         }
-    //     });
-
-    //     if (imageResponse.data.profiles.length > 0) {
-    //         this.setState({
-    //             imageUrl: imageBaseUrlLarge + imageResponse.data.profiles[0].file_path,
-    //             imageUrlSmall: imageBaseUrlSmall + imageResponse.data.profiles[0].file_path
-    //         });
-  
-    //     } else {
-    //         this.setState({
-    //             imageUrl: blankProfilePhoto,
-    //             imageUrlSmall: blankProfilePhoto
-    //         });
-    //     };
-    // }
-//end: creates default actor image for use in imageless films //
-
-//sorts film array and removes duplicates
-//     removeDuplicatesAndSort(array) {
-//         const mappedArray = array.map( (item) => {
-//             return item.id;
-//         })
-
-//         const filteredArray = array.filter( (item, index) => {
-//            return mappedArray.indexOf(mappedArray[index]) === array.indexOf(item);
-//          })
-        
-//         const sortedArray = filteredArray.sort( (a,b) => {
-//             return b.popularity - a.popularity;
-//         })
-
-//         return sortedArray;
-//     }
-
-//     getCreditsAndBio = async () => {
-//         const response2 = await axios.get(`https://api.themoviedb.org/3/person/${this.props.actorId}/combined_credits`, {
-//             params: {
-//                 api_key: key
-//             }
-//         })
-        
-//         const response3 = await axios.get(`https://api.themoviedb.org/3/person/${this.props.actorId}`, {
-//             params: {
-//                 api_key: key
-//             }
-//         })
-
-//         const array = this.removeDuplicatesAndSort(response2.data.cast);
-
-//         this.setState({filmArray: array}); //movies/tv shows in which this person was in the CAST!
-        
-//         if (response3.data.biography) {
-//             this.setState({
-//                 actorBio: response3.data.biography,
-//                 });
-//         } else {
-//             this.setState({
-//                 actorBio: 'Biography unavailable.',
-//             });
-//         }
-//     }
-
-//     changeFilms() {
-//         this.getCreditsAndBio();
-//         // this.changeDefaultImage();
-//     }
-
-//    onClick = async (event) => {
-//             // await this.setState({actorId: event.currentTarget.dataset.actorid});
-//             await 
-//             await this.setState({filmArray: []});
-//             this.changeFilms();
-//             this.setState({resultsDisplay: 'none'}); //hides SearchResults component
-
-//         };
 
     click = () => {
         this.props.matchState(this.props);
--- src/components/SearchResults2.js
+++ src/components/SearchResults2.js
@@ -5,10 +5,8 @@ const imageBaseUrlSmall = 'https://www.themoviedb.org/t/p/w150_and_h225_bestv2';
 const blankProfile = 'https://st3.depositphotos.com/4111759/13425/v/600/depositphotos_134255626-stock-illustration-avatar-male-profile-gray-person.jpg'
 class SearchResults2 extends React.Component {
 
-    state = {}
-    //adjust state here with onclick. pass this state to app.js with a callback prop, e.g. this.props.adjustState(this.state.state)
-
 //BIG QUESTION: Is there a way to pass the e.id below in another way other than assigning it to the id of the div??
+//yes, you can pass it via a props argument, or use the data html attribute
 
    mapArray(e) {
        if (e.profile_path) {
